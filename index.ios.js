b96d078e213c2b8cd9ae0d03183748e79b334548
Thomas Parslow <tom@almostobsolete.net>
2015-12-09 19:01:22 +0000

    Fix typo

--- index.ios.js
+++ index.ios.js
@@ -27,7 +27,7 @@ var NativeHTMLWebView = requireNativeComponent('AIBHTMLWebView', _HTMLWebView);
 var HTMLWebView = React.createClass({
   propTypes: {
     html: PropTypes.string.isRequired,
-    makeSafe: PropTypes.oneOfType([PropTypes.object, PropTypes.boolean]),
+    makeSafe: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
     onLink: PropTypes.func,
     style: View.propTypes.style,
     // Should this view adjust its height automatically to show its
